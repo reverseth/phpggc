05ac6921b037962c54c13b90d4822763d8de24a4
reverseth <quentin.laborie@gmail.com>
2025-10-26 23:09:22 +0100

    Add impaced versions

--- gadgetchains/CodeIgniter4/FD/3/chain.php
+++ gadgetchains/CodeIgniter4/FD/3/chain.php
@@ -4,7 +4,7 @@ namespace GadgetChain\CodeIgniter4;
 
 class FD3 extends \PHPGGC\GadgetChain\FileDelete
 {
-    public static $version = '';
+    public static $version = '4.5 <= 4.7+';
     public static $vector = '__destruct';
     public static $author = 'reverseth';
     public static $information = 'Actually, this POP chain writes (or replaces the content of) the choosen file with the following content : "<?php return array (0 => \'\',);" It is also possible to choose a string to write inside the array ("<?php return array (0 => \'<here>\',);"), but I did not find a way to execute it. Thus, I decided to push it as a FD, and not as a FW.';
